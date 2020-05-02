24e87e2c078fc84cd975c483eea8088c57eb87b6
DiHLoS <dihlos@gmail.com>
2020-05-02 19:39:23 +0300

    Set 'Bing Maps Streets' as default map

--- src/UI/main.js
+++ src/UI/main.js
@@ -48,7 +48,8 @@ $(function() {
 
 		map = new mapboxgl.Map({
 			container: 'map-view',
-			style: 'mapbox://styles/aliashraf/ck6lw9nr80lvo1ipj8zovttdx',
+			//style: 'mapbox://styles/aliashraf/ck6lw9nr80lvo1ipj8zovttdx',
+			style: 'mapbox://styles/mapbox/streets-v11',
 			center: [-73.983652, 40.755024], 
 			zoom: 12
 		});
